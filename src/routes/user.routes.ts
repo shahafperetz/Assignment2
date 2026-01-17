fc09cce496d9235acac684311d3857f93b9186c0
talsirbu <tals@clover.security>
2026-01-17 21:02:16 +0200

    add user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,7 +3,60 @@ import { getUsers, getUserById } from "../controllers/user.controller";
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: Users management
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: List of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 64f1c9a9e1b2c123456789ab
+ *                   username:
+ *                     type: string
+ *                     example: tal
+ *                   email:
+ *                     type: string
+ *                     example: tal@gmail.com
+ */
 router.get('/', getUsers);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Get user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User ID
+ *     responses:
+ *       200:
+ *         description: User found
+ *       404:
+ *         description: User not found
+ */
 router.get('/:id', getUserById);
 
 export default router;
