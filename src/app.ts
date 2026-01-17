ea46511413ced4f0a5d60841a3213a45fac6d8ff
talsirbu <tals@clover.security>
2026-01-17 14:44:47 +0200

    add comment routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoutes from './routes/user.routes';
 import postRoutes from './routes/post.routes';
+import commentRoutes from './routes/comment.routes';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
+app.use('/comments', commentRoutes);
 
 export default app;
