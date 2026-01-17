40ed6c05893f580fa6fbc240a1dc1582b4f85720
talsirbu <tals@clover.security>
2026-01-17 18:50:26 +0200

    delete and update comment

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createComment, getCommentByPost } from "../controllers/comment.controller";
+import { createComment, deleteComment, getCommentById, getCommentByPost, updateComment } from "../controllers/comment.controller";
 
 const router = Router();
 
 router.post('/', createComment);
 router.get('/', getCommentByPost);
+router.get('/:id', getCommentById);
+router.put('/:id', updateComment);
+router.delete('/:id', deleteComment);
 
 export default router;
